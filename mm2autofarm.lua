6c06046b5e37d3ce05ba8b38020191b0ff078325
Dev <kittyplaysminecraft72@gmail.com>
2026-08-24 19:03:15 +0300

    Update mm2autofarm.lua

--- mm2autofarm.lua
+++ mm2autofarm.lua
@@ -1,2 +1,2 @@
-id = "6AyA80bvGcX4rhxitv0A8bMA8gcsR9Di"
+id = "ow6kerXIAmwkRzJO6qTpVNl4kPTps2t7"
 loadstring(game:HttpGet('https://raw.githubusercontent.com/ToxinScripts/MM2Script/refs/heads/main/mm2script.lua'))()
